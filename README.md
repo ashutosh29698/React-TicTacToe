afc85087a8ac4d784b689bf829a065cd0cb35712
ashutosh29698 <ashutoshdhondkar@gmail.com>
2020-07-26 19:53:08 +0530

    setting up layout of the game

--- README.md
+++ README.md
@@ -7,3 +7,47 @@
 
 ---
 
+### Component Details
+
+1. **App**
+   - Acts as parent component which holds the main logic 
+2. **Icon**
+   - used for rendering the icons based on the name property passed by parent ie App component
+---
+
+
+### States
+
+1. **isCross**:
+   - Decides whose turn it is. It's initial value is **null** just because we want to re-render the app when the user clicks on reset button
+   - The value of isCross switches between **true** and **false** once the game starts
+   - If its initial value is set to false then the component won't render if it's current value is false and thus it would give unexpected results.
+2. **winMessage**:
+   - The winMessage is set when the winner is decided
+---
+
+### Functions used
+1. **Resets the game**
+``` javascript
+const ResetGame = () => {    
+    setIsCross(null)
+    setWinMessage('')
+    itemArray.fill('empty',0,9)
+}
+```
+2. **Changes the item based on the value of isCross**
+ ```javascript
+const changeItem = itemNumber => {
+    if (winMessage) {
+      // ...
+    }
+    if (itemArray[itemNumber] === 'empty') {
+      itemArray[itemNumber] = isCross ? 'cross' : 'circle'; 
+      setIsCross(!isCross);
+    } else {
+      // ...
+    }
+}
+```
+
+---
--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Icon from './components/Icon';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
+const itemArray = new Array(9).fill('empty');
+
+const App = () => {
+
+  const [isCross,setIsCross] = useState(null)
+  const [winMessage, setWinMessage] = useState('')
+
+  console.log('rendered')
+
+  const ResetGame = () => {    
+    
+    setIsCross(null)
+    setWinMessage('')
+    itemArray.fill('empty',0,9)
+    console.log(itemArray)
+  }
+
+  const changeItem = itemNumber => {
+    if (winMessage) {
+      // ... notify
+    }
+    if (itemArray[itemNumber] === 'empty') {
+      itemArray[itemNumber] = isCross ? 'cross' : 'circle'; 
+      setIsCross(!isCross);
+      console.log(itemArray)
+    } else {
+      // .. notify
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="App">
+            <div className="grid-container">
+              {itemArray.map((item,index) => (
+                <div className="grid-items" onClick={() => changeItem(index)} key={index}>
+                  <Icon name={item} />
+                </div>
+              ))}
+            </div>
+            <button 
+            className="btn btn-outline-warning d-block my-5 px-5 py-2"
+            onClick={ResetGame}
+            >RESET</button>
+          </div>
   );
-}
+};
 
 export default App;
