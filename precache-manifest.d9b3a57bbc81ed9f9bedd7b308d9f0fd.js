571a1f9a2ce6402f63c1a39202a6b94646351944
ashutosh29698 <ashutoshdhondkar@gmail.com>
2020-07-29 04:06:59 +0530

    Updates

---
+++ precache-manifest.d9b3a57bbc81ed9f9bedd7b308d9f0fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc114dfa1462015003bb7991f10b335e",
+    "url": "/React-TicTacToe/index.html"
+  },
+  {
+    "revision": "afe10194e48fcf3439df",
+    "url": "/React-TicTacToe/static/css/2.37865b2a.chunk.css"
+  },
+  {
+    "revision": "005f1a45b5e9c8c465d5",
+    "url": "/React-TicTacToe/static/css/main.4d34799a.chunk.css"
+  },
+  {
+    "revision": "afe10194e48fcf3439df",
+    "url": "/React-TicTacToe/static/js/2.b7107935.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React-TicTacToe/static/js/2.b7107935.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "005f1a45b5e9c8c465d5",
+    "url": "/React-TicTacToe/static/js/main.136d5e15.chunk.js"
+  },
+  {
+    "revision": "50fbc61b78907b8af51e",
+    "url": "/React-TicTacToe/static/js/runtime-main.fcb5e68f.js"
+  }
+]);
